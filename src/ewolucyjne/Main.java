f4f30a24204a218f45bc5020699b29dd7c7fc65e
Daniel Pogrebniak <duboti@gmail.com>
2012-05-08 19:40:56 +0200

    Uruchamianie Widoku w oddzielnym wątku

--- src/ewolucyjne/Main.java
+++ src/ewolucyjne/Main.java
@@ -1,11 +1,18 @@
 package ewolucyjne;
 
+import javax.swing.*;
+
 public class Main 
 {
 	public static void main(String[] args)
 	{
 		//Kontroler kontroler = new Kontroler();
 		//Widok widok = new Widok (kontroler);
-		new Widok ();
+		SwingUtilities.invokeLater(new Runnable() {
+		      public void run() 
+		      {
+		    	  new Widok ();
+		      }
+		});
 	}
 }
