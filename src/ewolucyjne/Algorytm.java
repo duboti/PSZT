836e815bf61ed590fda60b984032684093c15835
drah3n <kumpels@gmail.com>
2012-05-08 21:32:32 +0200

    Dodanie nowych parametrow w Algorytm Implementacja nowePokolenie Implementacja warunekStopu Implementacja wybierzNajlepszego Rozbudowa implementacji Selekcji

--- src/ewolucyjne/Algorytm.java
+++ src/ewolucyjne/Algorytm.java
@@ -8,11 +8,18 @@ public class Algorytm
 	private int lambda;
 	private int rodzajAlgorytmu;
 	private float wspolczynnikInterpolacji;
-	private ArrayList<Zakres> zakres;
-	private ArrayList<Float> sigmy;
+//	private ArrayList<Zakres> zakres;
+//	private ArrayList<Float> sigmy;
+	private int nrIteracji;
+	private float najlepszyWynik;
+	private int etapAlgorytmu;				//w planowo ma to byc enum
+	private int etapBezPoprawy;
+	private int maxIteracji;
 	String typOsobnika = "Punkt";
+	private int maxBezPoprawy;
 	
 	public ArrayList<Osobnik> populacja;
+	
 	//private ArrayList<Osobnik> potomkowie;
 	
 	Algorytm (int mi, int lambda, int rodzajAlgorytmu, float wspolczynnikInterpolacji, 
@@ -22,15 +29,21 @@ public class Algorytm
 		this.lambda = lambda;
 		this.rodzajAlgorytmu = rodzajAlgorytmu;
 		this.wspolczynnikInterpolacji = wspolczynnikInterpolacji;
-		this.zakres = zakres;
-		this.sigmy = sigmy;
+//		this.zakres = zakres;
+//		this.sigmy = sigmy;
+		this.nrIteracji = 0;
+		this.etapAlgorytmu = 0;		
+		this.etapBezPoprawy = 0;
+		this.maxIteracji = 50;
+		this.maxBezPoprawy = 5;
 		
 		for (int i=0; i<this.mi ; i++)
 		{
-			populacja.add(this.stworzOsobnika(typOsobnika, zakres, this.sigmy));
-			
+			populacja.add(this.stworzOsobnika(typOsobnika, zakres, sigmy));	
 		}
+		Collections.sort(populacja, new OsobnikComparator());
 		
+		najlepszyWynik = populacja.get(0).pobierzWartosc();
 	}
 	
 	Osobnik stworzPotomka(Osobnik mama, Osobnik tata) 
@@ -76,8 +89,25 @@ public class Algorytm
 	
 	ArrayList<Osobnik> stworzNastepnePokolenie() 
 	{
-		
-		return null;
+		if (etapAlgorytmu==1)	
+		{
+			ArrayList<Osobnik> nowePokolenie = new ArrayList<Osobnik>();
+			Random generator = new Random(); 
+			
+			for (int i=0; i<lambda ; i++)
+			{
+				nowePokolenie.add(stworzPotomka(populacja.get(generator.nextInt(mi)), 
+						populacja.get(generator.nextInt(mi))));	
+			}
+			
+			etapAlgorytmu = 2;
+				
+			return nowePokolenie;
+		}
+		else
+		{
+			return null;
+		}
 	} 
 	
 	void Selekcja(ArrayList<Osobnik> potomkowie) 
@@ -91,16 +121,32 @@ public class Algorytm
 				populacja.remove(i);
 			}
 		}
+		nrIteracji++;
+		
+		/**
+		 * W tym miejscu jest warunek który okresla minimalizacje badz maksymalizacje
+		 */
+		if (najlepszyWynik > populacja.get(0).pobierzWartosc())
+		{
+			etapBezPoprawy = 0;
+			najlepszyWynik = populacja.get(0).pobierzWartosc();
+		}
+		else
+		{
+			etapBezPoprawy++;
+		}
+		
+		etapAlgorytmu = 1;
 	}
 	
 	boolean warunekStopu() 
 	{
-		return false;
+		return (nrIteracji>=maxIteracji || etapBezPoprawy>=maxBezPoprawy);
 	}
 	
 	Osobnik wybierzNajlepszego() 
 	{
-		return null;
+		return populacja.get(0);
 	}
 	
 	
